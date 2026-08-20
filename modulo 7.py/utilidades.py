7d3407673dd8dc2ccc361f6a61fd370a64fb76e0
leandrocarmoxav-code <migueldiasmendes9@gmail.com>
2026-08-20 14:32:02 -0300

    update_lição20/08

---
+++ modulo 7.py/utilidades.py
@@ -0,0 +1,63 @@
+'''
+Potenciação
+
+Divisão
+
+Multiplicação
+
+Soma
+
+Subtração
+'''
+
+
+def soma(a, b):
+
+    return a + b
+
+
+def subtrair(a, b):
+
+    return a - b
+
+
+def multiplicar(a,b):
+
+    return a * b
+
+
+def dividir(a,b):
+
+    if b == 0:
+        return "Erro: Divisão por Zero não Permitida"
+    return a / b
+
+    
+def divisao_inteira(a, b):
+    
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a // b
+
+
+def resto_divisao(a, b):
+    
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a % b
+
+
+def potencia(base, expoente):
+    
+    return base ** expoente
+
+
+def calcular_media(lista_numeros):
+
+    if not lista_numeros:
+        return 0
+    return sum(lista_numeros) / len(lista_numeros)
+
+
+def e_par(numero):
+    return numero % 2 == 0
